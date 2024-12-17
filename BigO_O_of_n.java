eada1cbc89a28213c7c78145d0afbea1db7339f7
Kamalakannan S <128273085+Kamal1023@users.noreply.github.com>
2024-12-17 18:37:10 +0530

    Add files via upload

---
+++ BigO_O_of_n.java
@@ -0,0 +1,13 @@
+public class BigO_O_of_n {
+
+    public static void printItems(int n) {
+        for(int i=0; i<n;i++){
+            System.out.println(i);
+            
+        }
+    }
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}
