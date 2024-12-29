277c9c2c416c94c731c38943ed86b85f8b62182a
Kamalakannan S <128273085+Kamal1023@users.noreply.github.com>
2024-12-29 21:36:01 +0530

    Add files via upload

---
+++ Maps.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Maps {
+    public static void main(String[] args){
+
+        HashMap<String, Integer> empId = new HashMap<>();
+        empId.put("Jobin", 1);
+        empId.put("Bala", 2);
+        empId.put("Thathun", 3);
+        //empId.put(key, value);
+
+        System.out.println(empId);
+
+
+    }
+    
+}
