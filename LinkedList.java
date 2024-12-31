7d31b3f0d37f9dd7272bfca8d6f3fdadec07665c
Kamalakannan S <128273085+Kamal1023@users.noreply.github.com>
2024-12-31 23:50:41 +0530

    added append method

--- LinkedList.java
+++ LinkedList.java
@@ -21,11 +21,54 @@ public class LinkedList{
         length = 1;
 
     }
+
+    public void printList(){                    //function for printing our linked list
+        Node temp = head;                       //initial temp value is pointed to head
+        while (temp != null) {                  // this loop ensures that "temp" travels to the next node until it finds null value
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value){             //append method to add new values to the linked list
+        Node newNode = new Node(value);
+        if(length==0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node getHead(){
+        return head;
+    }
+    public Node getTail(){
+        return tail;
+    }
+    public int getLength(){
+        return length;
+    }
     
     public static void main(String[] args){
 
         LinkedList myLinkedList = new LinkedList(4);
         System.out.println(myLinkedList.head.value);
+
+        System.out.println("Head value: "+myLinkedList.getHead().value);
+        System.out.println("Tail value: "+myLinkedList.getTail().value);
+        System.out.println("Length: "+ myLinkedList.getLength());
+
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(1);
+
+
+        System.out.println("Linked List: ");
+        myLinkedList.printList();
+        
     }
     
 
